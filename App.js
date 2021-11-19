9704ea5c4fe9336d00175128e007423449c16ce1
silkykay <mlt6kps@virginia.edu>
2021-11-19 09:17:50 -0500

    new commit

--- App.js
+++ App.js
@@ -2,16 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+
+  constructor(props){ 
+    super(props) //have to call super since it extends 
+    this.state = {date: new Date}
+  }
+
+  componentDidMount(){
+    this.Timer = setInterval(() => {this.tick()}, 1000);
+  } //updates clock by 1 seconds
+
+  tick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.Timer)
+  }
+  render() {
+    return (
+      <View style={styles.container}>  
+        <Text>The current Time is :  
+          {this.state.date.toLocaleTimeString()}
+       </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({ 
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -19,3 +44,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
